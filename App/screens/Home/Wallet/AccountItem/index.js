4802cf95dfb8b24acbdd4afd6d02b176f0427717
devMarshall <onyendo52@gmail.com>
2019-12-28 16:44:53 +0100

    chore: scaffold wallet view

---
+++ App/screens/Home/Wallet/AccountItem/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, TouchableOpacity, Image } from 'react-native';
+import { RegularText, BoldText } from 'components/Text';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export default ({ backgroundColor, account_number, account_name, bank }) => {
+  return (
+    <View
+      style={{
+        width: '100%',
+        height: RFValue(88),
+        backgroundColor,
+        borderRadius: RFValue(10),
+        paddingEnd: RFValue(13.49),
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingStart: RFValue(22),
+        marginBottom: RFValue(14),
+      }}
+    >
+      <View>
+        <BoldText customstyle={{ color: '#FFF', fontSize: RFValue(25) }}>{account_number}</BoldText>
+        <RegularText customstyle={{ color: '#FFF', fontSize: RFValue(14) }}>
+          {account_name}
+        </RegularText>
+        <RegularText customstyle={{ color: '#FFF', fontSize: RFValue(14) }}>{bank}</RegularText>
+      </View>
+      <TouchableOpacity>
+        <Image
+          source={require('../../../../assets/icons/exit2.png')}
+          style={{ width: RFValue(20), height: RFValue(20) }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
--- App/screens/Home/Wallet/index.js
+++ App/screens/Home/Wallet/index.js
@@ -1,12 +1,76 @@
 import React from 'react';
-import { View } from 'react-native';
-import { RegularText } from 'components/Text';
+import { View, TouchableOpacity, ScrollView } from 'react-native';
+import { RegularText, MediumText, BoldText } from 'components/Text';
+import BaseStyles from 'theme/base';
+import AccountItem from './AccountItem';
+import { RFValue } from 'react-native-responsive-fontsize';
+import colors from 'theme/colors';
 
 export default props => {
   console.log(props);
   return (
-    <View>
-      <RegularText>Wallet View</RegularText>
+    <View style={BaseStyles.dashBackground}>
+      <View style={BaseStyles.dashTop}>
+        <View style={{ ...BaseStyles.dashSummaryBox, marginBottom: RFValue(17) }}>
+          <View>
+            <MediumText customstyle={{ fontSize: RFValue(12), color: 'rgba(255,255,255,0.8)' }}>
+              Balance
+            </MediumText>
+            <BoldText customstyle={{ fontSize: RFValue(35), color: '#FFF' }}>5,000</BoldText>
+          </View>
+          <TouchableOpacity style={BaseStyles.button2}>
+            <BoldText customstyle={{ fontSize: RFValue(10), color: colors.primary }}>
+              Withdraw
+            </BoldText>
+          </TouchableOpacity>
+        </View>
+        <View style={{ ...BaseStyles.dashSummaryBox, marginBottom: RFValue(17) }}>
+          <View>
+            <MediumText customstyle={{ fontSize: RFValue(12), color: 'rgba(255,255,255,0.8)' }}>
+              Pending amount
+            </MediumText>
+            <BoldText customstyle={{ fontSize: RFValue(35), color: '#FFF' }}>22,500</BoldText>
+          </View>
+        </View>
+        <View
+          style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}
+        >
+          <RegularText customstyle={{ color: 'rgba(255,255,255,0.6)', fontSize: RFValue(12) }}>
+            next payment date
+          </RegularText>
+          <BoldText customstyle={{ color: '#FFF', fontSize: RFValue(12) }}>
+            Monday, 28th August 2019
+          </BoldText>
+        </View>
+      </View>
+      <View style={BaseStyles.dashContent}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <AccountItem
+            backgroundColor={colors.primary}
+            account_number="0006566463"
+            account_name="Test Driver"
+            bank="Access Bank"
+          />
+          <AccountItem
+            backgroundColor={colors.primary}
+            account_number="0006566463"
+            account_name="Test Driver"
+            bank="Access Bank"
+          />
+          <AccountItem
+            backgroundColor={colors.primary}
+            account_number="0006566463"
+            account_name="Test Driver"
+            bank="Access Bank"
+          />
+          <AccountItem
+            backgroundColor={colors.primary}
+            account_number="0006566463"
+            account_name="Test Driver"
+            bank="Access Bank"
+          />
+        </ScrollView>
+      </View>
     </View>
   );
 };
