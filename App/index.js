d5e2fd17035a2cd044e6870e41c85dd39ce1c218
devMarshall <onyendo52@gmail.com>
2019-12-28 10:00:33 +0100

    chore: integrate firebase cloud messaging

--- App/index.js
+++ App/index.js
@@ -3,10 +3,21 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Stack } from 'shared/routes';
+import { firebase } from '@react-native-firebase/messaging';
+
 console.disableYellowBox = true;
 
 export default () => {
+  const fetchToken = async () => {
+    const fcm_token = await firebase.messaging().getToken();
+    console.log(fcm_token);
+  };
+
+  useEffect(() => {
+    fetchToken();
+  }, []);
+
   return <Stack />;
 };
