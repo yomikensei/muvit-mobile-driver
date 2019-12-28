7e3aff3942ad06220f8bbe20caf1e1a422920805
devMarshall <onyendo52@gmail.com>
2019-12-28 09:54:55 +0100

    chore: integrate firebase core

--- App/index.js
+++ App/index.js
@@ -3,14 +3,10 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { Stack } from "shared/routes";
+import React, { Component } from 'react';
+import { Stack } from 'shared/routes';
 console.disableYellowBox = true;
 
-export default class App extends Component {
-  render() {
-    return (
-      <Stack />
-    );
-  }
-}
+export default () => {
+  return <Stack />;
+};
