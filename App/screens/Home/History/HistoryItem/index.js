26027e6e2ca0d0fedca1e3c944a5d411f3bc0f36
devMarshall <onyendo52@gmail.com>
2020-01-16 15:18:31 +0100

    chore: revamp history item

--- App/screens/Home/History/HistoryItem/index.js
+++ App/screens/Home/History/HistoryItem/index.js
@@ -1,37 +1,54 @@
 import React from 'react';
-import {View} from 'react-native';
-import {MediumText, RegularText} from 'components/Text';
+import {Image, View} from 'react-native';
+import {MediumText} from 'components/Text';
 import {RFValue} from 'react-native-responsive-fontsize';
-import {currencyFormatter} from 'util';
-import moment from 'moment';
 import Colors from 'theme/colors';
 
 export default ({
   location_origin: { name: location_origin },
   location_destination: { name: location_destination },
-  createdAt,
-  bill,
 }) => {
   return (
     <View
       style={{
+        backgroundColor: Colors.secondary,
         width: '100%',
         borderRadius: RFValue(10),
-        paddingVertical: RFValue(13),
-        paddingHorizontal: RFValue(16),
-        backgroundColor: Colors.black,
-        marginBottom: RFValue(10),
+        paddingHorizontal: RFValue(15),
+        paddingVertical: RFValue(12),
+        marginBottom: RFValue(19),
       }}
     >
-      <MediumText customstyle={{ color: '#FFF', fontSize: RFValue(25) }}>{`₦ ${currencyFormatter(
-        bill
-      )}`}</MediumText>
-      <RegularText customstyle={{ color: '#FFF', fontSize: RFValue(14) }}>
-        {`${location_origin} to ${location_destination}`}
-      </RegularText>
-      <RegularText customstyle={{ color: '#FFF', fontSize: RFValue(14) }}>
-        {moment(createdAt).format('dddd, MMMM Do YYYY, h:mm a')}
-      </RegularText>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Image
+          resizeMode="contain"
+          source={require('../../../../assets/icons/dot.png')}
+          style={{ width: RFValue(28), height: RFValue(28), marginEnd: RFValue(7) }}
+        />
+        <View style={{ display: 'flex', width: '100%' }}>
+          <MediumText customstyle={{ fontSize: RFValue(10), color: '#B2B8BD' }}>From</MediumText>
+          <MediumText customstyle={{ fontSize: RFValue(16) }}>{location_origin}</MediumText>
+        </View>
+      </View>
+      <View
+        style={{
+          width: '100%',
+          height: RFValue(1),
+          backgroundColor: 'rgba(178,184,189,0.2)',
+          marginVertical: RFValue(10),
+        }}
+      />
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Image
+          resizeMode="contain"
+          source={require('../../../../assets/icons/dot.png')}
+          style={{ width: RFValue(28), height: RFValue(28), marginEnd: RFValue(7) }}
+        />
+        <View style={{ display: 'flex', width: '100%' }}>
+          <MediumText customstyle={{ fontSize: RFValue(10), color: '#B2B8BD' }}>To</MediumText>
+          <MediumText customstyle={{ fontSize: RFValue(16) }}>{location_destination}</MediumText>
+        </View>
+      </View>
     </View>
   );
 };
